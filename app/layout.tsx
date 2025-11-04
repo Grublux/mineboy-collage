9a1f1995679e76ad259b9dfd1e9d401b9d986506
Grublux <grublux@gmail.com>
2025-11-03 20:18:48 -0500

    Remove WalletProvider from layout to fix errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { WalletProvider } from "@/components/WalletProvider";
+// import { WalletProvider } from "@/components/WalletProvider";
 import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "NFT Collage",
+  title: "MineBoy Collage",
 };
 
 export default function RootLayout({
@@ -14,10 +14,11 @@ export default function RootLayout({
   return (
     <html lang="en" style={{ backgroundColor: '#000000' }}>
       <head>
-        <link rel="stylesheet" href="https://unpkg.com/@rainbow-me/rainbowkit@2/dist/index.css" />
+        {/* <link rel="stylesheet" href="https://unpkg.com/@rainbow-me/rainbowkit@2/dist/index.css" /> */}
       </head>
       <body style={{ backgroundColor: '#000000', margin: 0, padding: 0 }}>
-        <WalletProvider>{children}</WalletProvider>
+        {/* <WalletProvider>{children}</WalletProvider> */}
+        {children}
       </body>
     </html>
   );
